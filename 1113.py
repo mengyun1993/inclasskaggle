6bb11e4e4f14c39174419f75e64e30a7c9a7462e
airsplay <airsplay@cs.unc.edu>
2016-11-14 10:06:51 -0500

    SVM with both cat and number features

--- 1113.py
+++ 1113.py
@@ -1,12 +1,19 @@
 import csv
 import numpy as np
+import pandas as pd
+import math
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 data_train = []
-with open('test-data/pml_train.csv', 'rb') as f:
+train = pd.read_csv("../data/pml_train.csv")
+test = pd.read_csv("../data/pml_test_features.csv")
+train.describe(include = [np.number])
+
+"""
+with open('../data/pml_train.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         data_train.append(row) 
@@ -18,7 +25,7 @@ print data_train[1]
 
 
 data_test = []
-with open('test-data/pml_test_features.csv', 'rb') as f:
+with open('../data/pml_test_features.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         data_test.append(row) 
@@ -28,17 +35,19 @@ data_test = np.array(data_test)
 
 Xtrain = data_train[1:1000,-15:-1]
 y_train = data_train[1:1000,-1]
-
+Xtest = data_train[10000:10200,-15:-1]
+y_test = data_train[10000:10200,-1]
+#
 # be carefully with dataset, make sure you select the correct feature
-Xtest = data_test[1:,-14:]
+#Xtest = data_test[1:,-14:]
 #print Xtrain[0]
 #print Xtest[0]
 
 Xtrain=np.array(Xtrain,dtype=float)
 y_train=np.array(y_train,dtype=float)
 Xtest=np.array(Xtest,dtype=float)
-
-print Xtrain, y_train
+y_test = np.array(y_test,dtype=float)
+#print Xtrain, y_train
 
 
 model = SVR(C=1e3)
@@ -63,20 +72,20 @@ print('training mae:', mae)
 
 
 
-
 #y_ = predict(para0, Xtest)
 y_ = model.predict(Xtest)
 
 num_test = len(y_) 
 print num_test
-
-f = open('test-data/testout.csv', 'wt')
-try:
-    writer = csv.writer(f)
-    writer.writerow( ('id', 'loss') )
-    for i in range(num_test):
-        writer.writerow( ( data_test[i+1, 0], y_[i] ) )
-finally:
-    f.close()
+mae = (np.absolute(y_test - y_)).mean()
+print mae
+#f = open('../data/testout.csv', 'wt')
+#try:
+#    writer = csv.writer(f)
+#    writer.writerow( ('id', 'loss') )
+#    for i in range(num_test):
+#        writer.writerow( ( data_test[i+1, 0], y_[i] ) )
+#finally:
+#    f.close()
     
-    
+"""    
